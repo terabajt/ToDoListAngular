f1cc452b4519b6ffffb5bd93d587b2de19dbb047
Michał Pasynek <michalpasynek@gmail.com>
2022-08-12 14:48:52 +0200

    Create app with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
+import { getLocaleDateFormat } from '@angular/common';
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  editMode = false;
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string } | null = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: true,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: 'C Lista zadań zbudowana w Angularze.',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+  }
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+    };
+    this.tasks.push(task);
+    (this.taskName = ''), (this.taskDate = '');
+  }
+  switchEditMode() {
+    this.editMode = !this.editMode;
+  }
 }
--- src/app/app.module(Conflicted copy 2022-08-11 from laptop-michal).ts
+++ src/app/app.module(Conflicted copy 2022-08-11 from laptop-michal).ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import localePL from '@angular/common/locales/pl';
-import { FormsModule } from '@angular/forms';
 registerLocaleData(localePL);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
